ec227a499e0d91139145a0874366430a0fc038c3
Aki <Akifumi@net44-dhcp85.sfc.keio.ac.jp>
2012-07-10 12:01:49 +0900

    Added a new method.

--- src/OPGraphicPanel.java
+++ src/OPGraphicPanel.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.net.URL;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 
@@ -21,4 +23,9 @@ public class OPGraphicPanel extends JPanel {
 		g.drawString(s, this.getWidth()/2, this.getHeight()/2);
 	}
 	
+	public void printImage(Graphics g, URL u){
+		this.clean(g);
+		ImageIcon icon = new ImageIcon(u, null);
+		icon.paintIcon(null, g, icon.getIconWidth(), icon.getIconHeight());
+	}
 }
