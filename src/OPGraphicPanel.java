f740996038106877ae64359d2eabc1ed42d75ff3
Akifumi <Akifumi@Akifumi-no-MacBook-Pro.local>
2012-07-12 03:05:37 +0900

    Added a new member number.

--- src/OPGraphicPanel.java
+++ src/OPGraphicPanel.java
@@ -1,8 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import java.net.URL;
+import java.util.ArrayList;
 
-import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 
@@ -13,16 +12,20 @@ public class OPGraphicPanel extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 //	private OPEnemy[] enemy;
-	private OPObject myself;
 //	private OPCharacter[] character;
+	private ArrayList<OPObject> objects = new ArrayList<OPObject>();
 	
 	public OPGraphicPanel(){
-		myself = new OPObject(0, 0);
+
 	}
 	
 	public void paint(Graphics g){
 		clean(g);
-		myself.paint(g);
+
+		for (OPObject o : objects) {
+			o.move();
+			o.paint(g);
+		}
 	}
 
 	public void clean(Graphics g){
@@ -30,19 +33,19 @@ public class OPGraphicPanel extends JPanel {
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
 	}
 	
-	public void printString(Graphics g, String s){
-		g.setColor(new Color(0, 0, 0));
-		g.drawString(s, this.getWidth()/2, this.getHeight()/2);
-	}
+//	public void printString(Graphics g, String s){
+//		g.setColor(new Color(0, 0, 0));
+//		g.drawString(s, this.getWidth()/2, this.getHeight()/2);
+//	}
+//	
+//	public void printImage(Graphics g, URL u){
+//		this.clean(g);
+//		ImageIcon icon = new ImageIcon(u, null);
+//		icon.paintIcon(null, g, icon.getIconWidth(), icon.getIconHeight());
+//	}
 	
-	public void printImage(Graphics g, URL u){
-		this.clean(g);
-		ImageIcon icon = new ImageIcon(u, null);
-		icon.paintIcon(null, g, icon.getIconWidth(), icon.getIconHeight());
-	}
-	
-	public void addMyself(OPObject m){
-		myself = m;
+	public void addObject(OPObject o){
+		objects.add(o);
 	}
 
 }
