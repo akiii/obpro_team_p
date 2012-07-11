f57b7bf5e6f6a572c49fad7259c6eaa4455119a6
Akifumi <Akifumi@Akifumi-no-MacBook-Pro.local>
2012-07-12 01:22:53 +0900

    Could use mouse of game frame.

--- src/OPGameFrame.java
+++ src/OPGameFrame.java
@@ -1,4 +1,6 @@
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
@@ -7,7 +9,7 @@ import javax.swing.JFrame;
 import twitter4j.TwitterException;
 
 
-public class OPGameFrame extends JFrame {
+public class OPGameFrame extends JFrame implements MouseListener {
 
 	/**
 	 * 
@@ -36,6 +38,7 @@ public class OPGameFrame extends JFrame {
 		this.getContentPane().add(gPanel);
 		this.pack();
 		this.setVisible(true);
+		this.addMouseListener(this);
 		
 		this.twitter.login();
 //		this.gPanel.printImage(gPanel.getGraphics(), this.twitter.getCurrentUserIconURL());
@@ -55,6 +58,36 @@ public class OPGameFrame extends JFrame {
 		OPMyself myself = null;
 		myself = (OPMyself) this.factory.createIcon(100, 100, 100, 100, this.twitter.getCurrentUserIconURL());
 		this.gPanel.addMyself(myself);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("mouse clicked");
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
