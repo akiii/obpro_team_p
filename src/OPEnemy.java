0e85747eae26283b84351f7b69368210d42b0094
suguru1206 <s10430ss@pankaku.com>
2012-07-11 12:31:10 +0900

    AddOPEnemyVelocity

--- src/OPEnemy.java
+++ src/OPEnemy.java
@@ -9,6 +9,16 @@ public class OPEnemy extends OPIcon{
 	}
 
 	public void move() {
+		this.velocityX = (int) ((Math.random() * 10000)%3+1);
+		this.velocityY = (int) ((Math.random() * 10000)%3+1);
+		
+		if((int)(Math.random()*10000) % 2 == 0){
+			this.velocityX *= -1;
+		}
+		if((int)(Math.random()*10000) % 2 == 0){
+			this.velocityY *= -1;
+		}
+		
 		this.positionX += this.velocityX;
 		this.positionY += this.velocityY;
 	}
