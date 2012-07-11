9f503225762630a0f526d6bb20d4a018f9df4528
Akifumi <Akifumi@Akifumi-no-MacBook-Pro.local>
2012-07-12 03:06:46 +0900

    Edited a class.

--- src/OPGameFrame.java
+++ src/OPGameFrame.java
@@ -2,7 +2,6 @@ import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.IOException;
-import java.net.MalformedURLException;
 
 import javax.swing.JFrame;
 
@@ -19,47 +18,27 @@ public class OPGameFrame extends JFrame implements MouseListener {
 	/**
 	 * @param args
 	 */
-	private OPTwitter twitter = OPTwitter.sharedInstace();
+	private OPGameController controller = new OPGameController();
 	private OPGraphicPanel gPanel = new OPGraphicPanel();
-	
-	private OPObjectFactory factory;
-	
-	private float gameTime = 0;
-	
 	
 	public static void main(String[] args) throws TwitterException, IOException, InterruptedException {
 		// TODO Auto-generated method stub
 		new OPGameFrame();
 	}
 	
-	public OPGameFrame() throws TwitterException, IOException, InterruptedException{
-		this.setPreferredSize(new Dimension(300, 300));
+	public OPGameFrame() throws IllegalStateException, TwitterException, InterruptedException, IOException{
+		this.setPreferredSize(new Dimension(800, 500));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.getContentPane().add(gPanel);
 		this.pack();
 		this.setVisible(true);
 		this.addMouseListener(this);
-		
-		this.twitter.login();
-//		this.gPanel.printImage(gPanel.getGraphics(), this.twitter.getCurrentUserIconURL());
-//		this.twitter.getFriendsList(100);
-		
-		this.factory = new OPMyselfFactory();
-		
-		setStartingGraphic();
-		while(true){
-			gPanel.repaint();
-			Thread.sleep(30);
-			gameTime += 0.03;
-		}
+				
+		controller.setObjectsFirst(this, gPanel);
+		controller.runloop(this, this.gPanel);
+
 	}
 	
-	public void setStartingGraphic() throws MalformedURLException, IllegalStateException, TwitterException{
-		OPMyself myself = null;
-		myself = (OPMyself) this.factory.createIcon(100, 100, 100, 100, this.twitter.getCurrentUserIconURL());
-		this.gPanel.addMyself(myself);
-	}
-
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
