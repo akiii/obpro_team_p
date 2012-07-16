d7c6a6be80b7bd9b31308de76364f938e9394218
akiii <akiii@sfc.wide.ad.jp>
2012-07-16 15:08:00 +0900

    Edited a class.

--- src/OPGraphicPanel.java
+++ src/OPGraphicPanel.java
@@ -11,73 +11,27 @@ public class OPGraphicPanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private ArrayList<OPObject> objects = new ArrayList<OPObject>();
 	
 	public OPGraphicPanel(){
 
 	}
 	
-	public void paint(Graphics g){
+	public void paint(Graphics g, ArrayList<OPObject> objects){
 		clean(g);
 		for (OPObject o : objects) {
 			o.paint(g);
 		}
 	}
 	
-	public void moveObject(){
+	public void moveObjects(ArrayList<OPObject> objects){
 		for (OPObject o : objects) {
 			o.move();
-		}
-	}
-	
-	public void checkRemoveObject(OPGameFrame f){
-		ArrayList<OPObject> removeObjects = new ArrayList<OPObject>();
-		for (OPObject o : objects) {
-			if (o.positionX < -100 || o.positionX > f.getWidth()+100 
-					|| o.positionY < -100 || o.positionY > f.getHeight()+100) {
-				removeObjects.add(o);
-			}
-		}
-		for (OPObject o : removeObjects) {
-			objects.remove(o);
-		}
-	}
-	
-	public void checkConflict() {
-		for (OPObject o : objects) {
-			if(o.getEnemy() != null){
-				for (OPObject p : objects) {
-					if(p.getCharacter() != null) {
-						if (o.positionX - o.width/2 < p.positionX + p.width/2 && 
-							o.positionX + o.width/2 > p.positionX - p.width/2 && 
-							o.positionY - o.height/2 < p.positionY + p.height/2 && 
-							o.positionY + o.height/2 > p.positionY - p.height/2) {
-							System.out.println("a");
-						}
-					}
-				}
-			}
 		}
 	}
 
 	public void clean(Graphics g){
 		g.setColor(new Color(255, 255, 255));
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
-	}
-	
-//	public void printString(Graphics g, String s){
-//		g.setColor(new Color(0, 0, 0));
-//		g.drawString(s, this.getWidth()/2, this.getHeight()/2);
-//	}
-//	
-//	public void printImage(Graphics g, URL u){
-//		this.clean(g);
-//		ImageIcon icon = new ImageIcon(u, null);
-//		icon.paintIcon(null, g, icon.getIconWidth(), icon.getIconHeight());
-//	}
-	
-	public void addObject(OPObject o){
-		objects.add(o);
 	}
 
 }
